76db72cb297b4a691bba0e067b71ee569fb63114
Michaela Prochazkova <prochazkovamichaela95@gmail.com>
2023-02-01 20:26:10 +0100

    done

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-// tady je místo pro náš program
+/* Zadání domácího úkolu
+ • Vytvořte tlačítko, při jehož stisknutí se změní barva Čtvereček na zelenou.
+ • Upravte kód tak, aby se výsledek funkce secti() zobrazil v prvku s ID vysledek až po stiknutí nově přidaného tlačítka "Zobraz výsledek".
+ • Uprav funkci upozorni() tak, aby se navíc vypisoval text i do konzole a také nahradil text v elementu Čtvereček.
 
+ */
+
+function zobrazVysledek() {
 document.querySelector("#vysledek").innerHTML = secti(4, 5);
+}
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -18,4 +25,13 @@ function secti(a, b) {
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log ("Gratulace, právě jsi spustila tuto funkci!")
+  let ctverecek = document.querySelector(".ctverecek")
+  ctverecek.innerHTML = "Gratulace"
 }
+
+function zmenBarvu() {
+let ctverecek = document.querySelector(".ctverecek")
+ctverecek.style.backgroundColor = "green"
+}
+
